abc3a5ee7b49c5bffcc2e1402b808e9ff14afbde
Youngmi Lee <abinding@gmail.com>
2018-05-10 17:06:11 -0500

    3rd commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,53 +2,53 @@
 var friendsArray = [
     {
         "name":"Henry",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "photo":"https://johnsonlaird.com/img/1243/full/1/20180118160814877_1243.jpg",
+        "scores":[5, 3, 3, 3, 5, 1, 2, 2, 4, 4]
       },
       {
         "name":"Linda",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "photo":"https://res.cloudinary.com/demo/image/upload/w_400,h_400,c_crop,g_face,r_max/w_200/lady.jpg",
+        "scores":[2, 3, 3, 4, 5, 1, 2, 5, 5, 5]
       },
       {
         "name":"Jordan",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "photo":"https://johnsonlaird.com/img/1243/full/1/20180118160814877_1243.jpg",
+        "scores":[1, 1, 2, 4, 2, 1, 2, 2, 4, 2]
       },
       {
         "name":"John",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "photo":"https://johnsonlaird.com/img/1243/full/1/20180118160814877_1243.jpg",
+        "scores":[3, 3, 4, 4, 5, 5, 2, 5, 4, 1]
       },    
       {
         "name":"Melissa",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "photo":"https://res.cloudinary.com/demo/image/upload/w_400,h_400,c_crop,g_face,r_max/w_200/lady.jpg",
+        "scores":[3, 1, 1, 1, 5, 1, 4, 5, 4, 4]
       },
       {
         "name":"Peter",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "photo":"https://johnsonlaird.com/img/1243/full/1/20180118160814877_1243.jpg",
+        "scores":[5, 1, 5, 4, 5, 1, 2, 3, 2, 2]
       },
       {
         "name":"Sam",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "photo":"https://johnsonlaird.com/img/1243/full/1/20180118160814877_1243.jpg",
+        "scores":[1, 5, 4, 4, 5, 5, 3, 5, 2, 1]
       },
       {
         "name":"Matt",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "photo":"https://johnsonlaird.com/img/1243/full/1/20180118160814877_1243.jpg",
+        "scores":[1, 1, 2, 3, 3, 4, 5, 1, 2, 4]
       },
       {
         "name":"Ally",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "photo":"https://res.cloudinary.com/demo/image/upload/w_400,h_400,c_crop,g_face,r_max/w_200/lady.jpg",
+        "scores":[2, 3, 1, 1, 2, 3, 4, 5, 1, 2]
       },
       {
         "name":"Sally",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "photo":"https://res.cloudinary.com/demo/image/upload/w_400,h_400,c_crop,g_face,r_max/w_200/lady.jpg",
+        "scores":[2, 3, 4, 5, 1, 2, 3, 4, 5, 1]
       }
   ];
   
