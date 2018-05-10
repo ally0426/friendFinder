041ff0672fa1062c0a5e3ace9df7bc9645ecd8cb
Youngmi Lee <abinding@gmail.com>
2018-05-10 16:58:03 -0500

    second commit

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+
+var friendsArray = [
+    {
+        "name":"Henry",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        "name":"Linda",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        "name":"Jordan",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        "name":"John",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },    
+      {
+        "name":"Melissa",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        "name":"Peter",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        "name":"Sam",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        "name":"Matt",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        "name":"Ally",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        "name":"Sally",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
+  
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
+
+const app = express();
 
 var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function() {
   console.log(`The server is starting on port ${PORT}`);
-}
+});
