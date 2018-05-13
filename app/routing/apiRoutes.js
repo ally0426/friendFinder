0d8d63b2ea111716812160d78b03d9cf5839ecac
Youngmi Lee <abinding@gmail.com>
2018-05-13 17:51:11 -0500

    final commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,70 @@
 var friendList = require("../data/friends");
 var path = require("path");
 
-module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
     res.json(friendList);
-  });  
-          
-  app.post("/api/friends", function(req, res) {
-    friendList.push(req.body);
-  });          
-}
+  });
+
+  app.post("/api/friends", function (req, res) {
+    console.log("req.body", req.body);
+
+    var newFriendScores = req.body.scores;
+    console.log("newFriendScores", newFriendScores);
+
+    var match;
+    var currentBest = 100;
+    var differenceArray = [];
+    var bestIndex;
+
+    console.log("newFriendScores.length", newFriendScores.length);
+    console.log("friendList.length", friendList.length);
+    console.log("frinedList", friendList);
+
+    for (var i = 0; i < friendList.length; i++) {
+      for (var j = 0; j < 10; j++) {
+        console.log("parseInt(newFriendScores[i]", parseInt(newFriendScores[j]));
+        console.log("friendList.scores", friendList.scores);
+
+        var currentDifference = parseInt(newFriendScores[j]) - friendList[i].scores[j];
+        console.log("currentDifference", currentDifference);
+
+        differenceArray.push(Math.abs(currentDifference));
+      }
+
+      var difference = differenceArray.reduce(add, 0); // needs function add()
+      console.log("final difference*********************", difference);
+      console.log("friendList[i]", friendList[i]);
+      console.log("currentBest", currentBest);
+      if (difference < currentBest) {
+       
+        // var matchName = friendList[i].name;
+        // var matchPic = friendList[i].photo;
+
+        
+         
+        console.log("matchName", friendList[i].name);
+        console.log("matchPic", friendList[i].photo);
+        console.log("i", i);
+        bestIndex = i;
+        console.log("index@@@@@@@@@@@@@@@@@@@", bestIndex);
+
+
+        currentBest = difference;
+        differenceArray = [];
+      }
+      else {
+        differenceArray = [];
+      }
+
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log("friendList[bestIndex]", friendList[bestIndex]);
+
+    res.json(friendList[bestIndex]);
+
+  });
+};
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -7,6 +8,8 @@ var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// app.use(express.static(path.join(__dirname, "../public")));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
